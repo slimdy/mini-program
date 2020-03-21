ee764234b5b661d5ee93191d453707c37bdd050b
slimdy <lfwoaizkx@163.com>
2020-03-21 14:47:12 +0800

    finish home page

--- pages/home/childComps/sl-recommend/sl-recommend.js
+++ pages/home/childComps/sl-recommend/sl-recommend.js
@@ -14,13 +14,19 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+      flag:false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    imageLoad(){
+      if (!this.data.flag){
+        this.data.flag = true // have not to change the data by using setData,course we don't need to refresh the page
+        this.triggerEvent("imageLoad",{},{})
+      }
 
+    }
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -15,7 +15,10 @@ Page({
       'new':{page:0,list:[]},
       'sell':{page:0,list:[]}
     },
-    currentType: "pop"
+    currentType: "pop",
+    isShow:false,
+    isFixed:false,
+    tabControlTop:0
 
   },
   /**
@@ -25,16 +28,92 @@ Page({
   titleClick(event){
     // const cpn = this.selectComponent("class/id") 父组件修改子组件的data
     // cpn.setData({})
-    console.log();
     const currentType = Object.keys(this.data.goods)[event.detail.index]
     this.setData({
       currentType
     })
+    console.log(event)
+    
+    switch(event.currentTarget.id){
+      case "tab-control":
+        this.selectComponent("#tab-control1").setData({
+          currentIndex: event.detail.index
+        })
+        break
+      case "tab-control1":
+        this.selectComponent("#tab-control").setData({
+          currentIndex: event.detail.index
+        })
+        break
+      
+    }
 
+    // let id_values = [""]
+    // let _id = event.currentTarget.id
+
+    //
+
+  },
+  backtop(){
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration:500
+    })
+  },
+
+  imageLoad(){
+    
+    wx.createSelectorQuery().select('#tab-control').boundingClientRect(rect=> {
+      // rect.id      // 节点的ID
+      // rect.dataset // 节点的dataset
+      // rect.left    // 节点的左边界坐标
+      // rect.right   // 节点的右边界坐标
+      // rect.top     // 节点的上边界坐标
+      // rect.bottom  // 节点的下边界坐标
+      // rect.width   // 节点的宽度
+      // rect.height  // 节点的高度
+      this.setData({
+        tabControlTop:rect.top
+      })
+    }).exec()
   },
   onReachBottom(){
     console.log("上拉加载更多")
-    // this._getGoodData(this.data.currentType)
+    this._getGoodData(this.data.currentType)
+  },
+  onPageScroll: function (e) {
+    //console.log(e)
+    let scrollTop = e.scrollTop
+    if (scrollTop < this.data.tabControlTop && this.data.isShow){
+     
+      let isShow = false
+      this.setData({
+        isShow
+      })
+    }
+    if (scrollTop >= this.data.tabControlTop && !this.data.isShow){
+      let isShow = true
+      this.setData({
+        isShow
+      })
+    }
+
+    //tabcontrol sticky
+    if(scrollTop >= this.data.tabControlTop && !this.data.isFixed){
+   
+      this.setData({
+        isFixed: !this.data.isFixed
+      })
+    }
+
+    if (scrollTop < this.data.tabControlTop && this.data.isFixed) {
+
+      this.setData({
+        isFixed: !this.data.isFixed
+      })
+    }
+
+   
   },
 
   /**
@@ -43,25 +122,61 @@ Page({
    */
   _getAllData(){
     //banner recommend
+
   
-    getMultiData().then(res=>{
-      const data = res.data.data
+    const promise_list = [getMultiData()]
+    const args = []
+  
+    Object.getOwnPropertyNames(this.data.goods).forEach(key=>{
+      let page = this.data.goods[key].page + 1
+      promise_list.push(getGoodData(page, key))
+      args.push({page,key})
+    })
+    wx.showLoading({
+      title: '加载中',
+      mask:true
+    })
+    console
+    Promise.all(promise_list).then(values=> {
+      console.log(values)
+      const data = values[0].data.data
       const banners = data.banner.list
-      const recommends  =data.recommend.list
+      const recommends = data.recommend.list
       this.setData({
         banners,
         recommends
       })
-    }).catch(err=>console.log(err))
-  
-    Object.getOwnPropertyNames(this.data.goods).forEach(key=>{
-      this._getGoodData(key)
-    })
-    
+      console.log("number1 ok")
+      for(let index in args){
+        index = parseInt(index)
+        let key = args[index].key
+        let page = args[index].page
+        const res = values[index+1]
+        const oldList = this.data.goods[key].list
+        oldList.push(...res.data.data.list)
+        const typeKey = 'goods.' + key + '.list'
+        const pageKey = `goods.${key}.page`
+        this.setData({
+          [typeKey]: oldList,
+          [pageKey]: page
+        })
+      }
+      wx.hideLoading()
+
+
+    }).catch(err => {
+          wx.hideLoading()
+          console.log(err)
+        
+    });
 
   },
   _getGoodData(key){
-    let page = this.data.goods[key].page+1
+      wx.showLoading({
+        title: '加载中',
+        mask: true
+      })
+      let page = this.data.goods[key].page+1
       
       getGoodData(page,key).then(res=>{
         // 该对象中的某个属性
@@ -69,13 +184,16 @@ Page({
         oldList.push(...res.data.data.list)
         const typeKey  = 'goods.'+ key +'.list'
         const pageKey  = `goods.${key}.page`
-        console.log(typeKey,pageKey,oldList,page)
         this.setData({
           [typeKey]: oldList,
           [pageKey]:page
         })
+        wx.hideLoading()
 
-      }).catch(err=>console.log(err))
+      }).catch(err=>{
+        console.log(err)
+        wx.hideLoading()
+      })
 
   },
 
@@ -84,6 +202,10 @@ Page({
    */
   onLoad: function (options) { // 执行一次
     //options里有其他页面传递的数据
+    wx.showLoading({
+      mask:true,
+      title: '加载中',
+    })
     this._getAllData()
 
   },
@@ -121,20 +243,8 @@ Page({
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () { 
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
+ 
   /**
    * 用户点击右上角分享
    */
